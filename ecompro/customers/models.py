e93e9ab3ea01dd752db16d344d4b2d0456c3a934
Amal Jith <amal4jith@gmail.com>
2024-05-12 14:51:50 +0530

    data model design completed

--- ecompro/customers/models.py
+++ ecompro/customers/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200)
     address = models.TextField()
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
-    phone = models.ImageField(upload_to='/media')
+    phone = models.IntegerField(null=False)
     priority = models.IntegerField(default=0)
     delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at = models.DateTimeField(auto_now_add=True)
--- ecompro/orders/models.py
+++ ecompro/orders/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 from customers.models import Customer
+from products.models import Product
 
 # Create your models here.
-class Customer(models.Model):
+class Order(models.Model):
     LIVE = 1
     DELETE = 0
     DELETE_CHOICES = ((LIVE,'live'),(DELETE,'delete'))
-
-    owner = models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
-    address = models.TextField()
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
-    phone = models.ImageField(upload_to='/media')
-    priority = models.IntegerField(default=0)
+    CART_STAGE = 0
+    ORDER_CONFIRMED = 1
+    ORDER_PROCESSED = 2
+    ORDER_DELIVERED = 3
+    ORDER_REJECTED = 3
+    STATIC_CHOICE = ((ORDER_PROCESSED,"ORDER_PROCESSED"),
+                     (ORDER_DELIVERED,"ORDER_DELIVERED"),
+                     (ORDER_REJECTED,"ORDER_REJECTED")
+                    )
+    
+    order_status = models.IntegerField(choices=STATIC_CHOICE,default=CART_STAGE)
+    owner = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,related_name='orders')
     delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return self.name
+        return self.owner
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product,related_name='added_cart',on_delete=models.SET_NULL,null=True)
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(Order,on_delete=models.CASCADE,related_name='added_items')
+
