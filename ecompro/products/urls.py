a8ba55971f59b2495da180462561ee542f03a005
Amal Jith <amal4jith@gmail.com>
2024-05-12 21:50:49 +0530

    template layout setup

--- ecompro/products/urls.py
+++ ecompro/products/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('products',views.product,name="product"),
+    
+    path('view-product',views.viewproduct,name="viewproduct"),
 ]
--- ecompro/products/views.py
+++ ecompro/products/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index-2.html')
+
+
+def product(request):
+    return render(request,'product.html')
+
+
+
+def viewproduct(request):
+    return render(request,'singleproduct.html')
